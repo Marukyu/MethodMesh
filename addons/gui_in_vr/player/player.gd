c5b9b2aef9ec7b9e79d3e4b95d30411aa17ed1e7
Marukyu <434348-Marukyu@users.noreply.gitlab.com>
2021-05-28 14:26:56 +0200

    Set up initial call graph and add support for non-VR mode

--- addons/gui_in_vr/player/player.gd
+++ addons/gui_in_vr/player/player.gd
@@ -4,13 +4,15 @@ extends ARVROrigin
 const XRServer = ARVRServer
 
 var _ws := 1.0
+var enableVR := false
 
 onready var _camera = $XRCamera
 onready var _camera_near_scale = _camera.near
 onready var _camera_far_scale = _camera.far
 
+var GUIType = preload("res://addons/gui_in_vr/gui.tscn")
 
-func _ready():
+func initVR():
 	var vr = XRServer.find_interface("OpenVR")
 	if vr and vr.initialize():
 		var viewport = get_viewport()
@@ -24,7 +26,17 @@ func _ready():
 		get_tree().quit()
 
 
-func _process(_delta):
+func _ready():
+	if enableVR:
+		initVR()
+	else:
+		get_parent().find_node("GUIPanel3D").hide()
+		get_parent().call_deferred("add_child", GUIType.instance())
+		Engine.target_fps = 60
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+func processVR():
 	var new_ws = XRServer.world_scale
 	if _ws != new_ws:
 		_ws = new_ws
@@ -33,3 +45,39 @@ func _process(_delta):
 		var child_count = get_child_count()
 		for i in range(3, child_count):
 			get_child(i).scale = Vector3.ONE * _ws
+
+
+func _input(event):
+	if !enableVR:
+		if Input.get_mouse_mode() != Input.MOUSE_MODE_CAPTURED && event is InputEventMouseButton:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		if Input.get_mouse_mode() != Input.MOUSE_MODE_VISIBLE && event is InputEventMouseMotion:
+			_camera.rotation_degrees.y -= (event.relative.x * 0.15)
+			_camera.rotation_degrees.x -= (event.relative.y * 0.2)
+
+
+func _notification(event):
+	if !enableVR:
+		if event == MainLoop.NOTIFICATION_WM_FOCUS_OUT:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+
+func processNonVR(delta):
+	var speed = delta * 2
+	if Input.is_key_pressed(KEY_SHIFT):
+		speed *= 3
+	if Input.is_action_pressed("move_right"):
+		_camera.translate_object_local(Vector3(speed, 0, 0))
+	if Input.is_action_pressed("move_left"):
+		_camera.translate_object_local(Vector3(-speed, 0, 0))
+	if Input.is_action_pressed("move_forward"):
+		_camera.translate_object_local(Vector3(0, 0, -speed))
+	if Input.is_action_pressed("move_backward"):
+		_camera.translate_object_local(Vector3(0, 0, speed))
+
+
+func _process(delta):
+	if enableVR:
+		processVR()
+	else:
+		processNonVR(delta)
